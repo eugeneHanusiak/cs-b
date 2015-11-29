5f8f3a5daeeeebb0ce0532c9d0481446df4002dd
shpp-eganusyak <g_e_v@mail.ua>
2015-11-29 23:48:21 +0200

    first commit

---
+++ TreeFractal/src/tree.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "gwindow.h"
+using namespace std;
+
+double SIDE_SIZE = 120; //size of the branch at the start of program
+const int cycleCount = 7; // density of tree branches
+
+void drawFractal(GWindow gw,GPoint root,double SIDE_SIZE,int cycleCount,double theta){
+    if(cycleCount <2){
+        gw.setColor("GREEN");
+    }else{
+        gw.setColor("GRAY");
+    }
+    //main branch
+    GPoint top = gw.drawPolarLine(root,SIDE_SIZE,theta);
+
+    //drawing polar lines of the main polar line by using recursion
+    if(cycleCount >=0){
+        drawFractal(gw,top,SIDE_SIZE/1.3,cycleCount-1,theta - 30);
+        drawFractal(gw,top,SIDE_SIZE/1.3,cycleCount-1, theta);
+        drawFractal(gw,top,SIDE_SIZE/1.3,cycleCount-1, theta + 30);
+    }
+}
+
+int main() {
+    //open a window
+    GWindow gw (1000, 600);
+    //main branch position by horizon;
+    double theta = 90;
+
+    //the sun
+    for(int i(0);i<50;i++){
+        gw.setColor("YELLOW");
+        gw.drawPolarLine(100,100,150,i*7.2);
+
+    }
+    gw.setColor("ORANGE");
+    gw.fillOval(50,50,100,100);
+
+    //star point of the tree(root)
+    GPoint root(gw.getWidth()/2,gw.getHeight());
+    drawFractal(gw,root,SIDE_SIZE,cycleCount,theta);
+    return 0;
+}
+
